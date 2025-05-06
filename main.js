67afda18ec261f97e1ea38198385463423829770
pedrobocatto <145362033+pedrobocatto@users.noreply.github.com>
2025-05-06 11:29:45 -0300

    060525aula

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelector(".botao");
+
+for (let i = 0; i <botoes.lenght; i++) {
+    botoes [1].onclickm = function () {
+        for (let j = 0; j <botoes.lenght; j++) {
+            botoes[j].classlist.remove("ativo");
+        }
+
+        botoes[i].classlist.add("ativo");
+    }
+}
