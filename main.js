c3536ce582c9b9d3f268ec905ace9da81417c58b
pedrobocatto <145362033+pedrobocatto@users.noreply.github.com>
2025-05-13 11:31:01 -0300

    1305202503

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelector(".botao");
 
 for (let i = 0; i <botoes.lenght; i++) {
-    botoes [1].onclickm = function () {
+    botoes [1].onclick = function () {
         for (let j = 0; j <botoes.lenght; j++) {
             botoes[j].classlist.remove("ativo");
         }
