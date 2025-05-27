c8c30b9f4ac201edd26d8ad16440fc4dc60fe827
pedrobocatto <145362033+pedrobocatto@users.noreply.github.com>
2025-05-27 11:31:22 -0300

    27062025

--- main.js
+++ main.js
@@ -15,4 +15,25 @@ for (let i = 0; i <botoes.lenght; i++) {
 }
 
 const contadores = document.querySelectorAll ("contador");
-const tempoObjetivo = new Date ("2025-06-20t00:00:00");
+const tempoObjetivo1 = new Date ("2025-06-20T00:00:00");
+const tempoObjetivo2 = new Date ("2025-06-25T00:00:00");
+const tempoObjetivo3 = new Date ("2025-06-30T00:00:00");
+const tempoObjetivo4 = new Date ("2025-07-05T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+
+function calculaTempo (tempoObjetivo) {
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor (minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+}
